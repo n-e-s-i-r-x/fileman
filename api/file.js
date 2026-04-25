2e25a812ee62d489454299cc7010931e35e739a6
n-e-s-i-r-x <rakutadrakutan@gmail.com>
2026-04-26 07:44:30 +0800

    Update file.js

--- api/file.js
+++ api/file.js
@@ -2,11 +2,11 @@
 // Vercel Serverless Function
 // GET /api/files → returns { files: [...] }
 
-import { UTApi } from 'uploadthing/server';
+const { UTApi } = require('uploadthing/server');
 
 const utapi = new UTApi();
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
